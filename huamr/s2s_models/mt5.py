205a34e9ea74f645e059030b74286117b6f0cf5c
Botond Barta <b.botond25@gmail.com>
2024-08-23 09:49:22 +0200

    do not use legacy mt5 tokenizer

--- huamr/s2s_models/mt5.py
+++ huamr/s2s_models/mt5.py
@@ -1,7 +1,6 @@
 import logging
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainer, \
-    Seq2SeqTrainingArguments, EarlyStoppingCallback
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from typing_extensions import override
 
 from huamr.s2s_models.base_model import S2SBaseModel
@@ -13,31 +12,31 @@ class MT5(S2SBaseModel):
     def __init__(self, config):
         super().__init__(config)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_checkpoint)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_checkpoint)
+        self._tokenizer = AutoTokenizer.from_pretrained(self.config.model_checkpoint, legacy=False)
+        self._model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_checkpoint)
 
     @override
     def get_model(self):
-        return self.model
+        return self._model
 
     @override
     def get_tokenizer(self):
-        return self.tokenizer
+        return self._tokenizer
 
     @override
     def process_data_to_model_inputs(self, batch):
-        inputs = self.tokenizer(batch['sentence'],
-                                padding='max_length',
-                                max_length=self.config.max_input_length,
-                                truncation=True)
-
-        outputs = self.tokenizer(text_target=batch['amr_graph'],
+        inputs = self._tokenizer(batch['sentence'],
                                  padding='max_length',
-                                 max_length=self.config.max_output_length,
+                                 max_length=self.config.max_input_length,
                                  truncation=True)
 
+        outputs = self._tokenizer(text_target=batch['amr_graph'],
+                                  padding='max_length',
+                                  max_length=self.config.max_output_length,
+                                  truncation=True)
+
         outputs["input_ids"] = [
-            [(l if l != self.tokenizer.pad_token_id else -100) for l in label] for label in outputs["input_ids"]
+            [(l if l != self._tokenizer.pad_token_id else -100) for l in label] for label in outputs["input_ids"]
         ]
         inputs['labels'] = outputs['input_ids']
         return inputs

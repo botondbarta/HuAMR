4f777d71b88ad06465539a8d5a0c7655b0f338dc
Botond Barta <b.botond25@gmail.com>
2024-09-26 09:44:43 +0200

    support llama 3.x models, do not set eos token

--- huamr/llm_models/llama.py
+++ huamr/llm_models/llama.py
@@ -6,7 +6,6 @@ from huamr.llm_models.base_model import LLMBaseModel
 class LlamaModel(LLMBaseModel):
     @override
     def set_special_tokens(self, model_name, tokenizer):
-        if "llama-3.1" in model_name.lower():
+        if "llama-3." in model_name.lower():
             tokenizer.pad_token = '<|finetune_right_pad_id|>'
-            tokenizer.eos_token = '<|eot_id|>'
             tokenizer.bos_token = '<|begin_of_text|>'

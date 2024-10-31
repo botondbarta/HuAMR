78df975b89ed1794eac91cb9c4a9445ad9665510
Botond Barta <b.botond25@gmail.com>
2024-10-31 18:42:13 +0100

    use correct qwen2 modelname

--- huamr/s2s_models/encoderdecoder.py
+++ huamr/s2s_models/encoderdecoder.py
@@ -13,10 +13,8 @@ class AMREncoderDecoderModel(S2SBaseModel):
     def __init__(self, config: DotMap):
         super().__init__(config)
 
-
         self.bert_tokenizer = BertTokenizer.from_pretrained('SZTAKI-HLT/hubert-base-cc')
-        self.coder_tokenizer = AutoTokenizer.from_pretrained("qwen2.5-coder")
-
+        self.coder_tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-1.5B")
 
         self._model = EncoderDecoderModel.from_encoder_decoder_pretrained(
             'SZTAKI-HLT/hubert-base-cc',
@@ -34,14 +32,14 @@ class AMREncoderDecoderModel(S2SBaseModel):
     @override
     def process_data_to_model_inputs(self, batch):
         inputs = self.bert_tokenizer(batch['sentence'],
-                                 padding='max_length',
-                                 max_length=self.config.max_input_length,
-                                 truncation=True)
+                                     padding='max_length',
+                                     max_length=self.config.max_input_length,
+                                     truncation=True)
 
         outputs = self.coder_tokenizer(text_target=batch['amr_graph'],
-                                  padding='max_length',
-                                  max_length=self.config.max_output_length,
-                                  truncation=True)
+                                       padding='max_length',
+                                       max_length=self.config.max_output_length,
+                                       truncation=True)
 
         outputs["input_ids"] = [
             [(l if l != self.coder_tokenizer.pad_token_id else -100) for l in label] for label in outputs["input_ids"]

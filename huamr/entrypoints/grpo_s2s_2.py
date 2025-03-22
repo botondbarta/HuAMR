6ba203d8378d1244a3a0c13423be1bbcee6d2594
Botond Barta <b.botond25@gmail.com>
2025-03-22 16:34:10 +0100

    add missing import

--- huamr/entrypoints/grpo_s2s_2.py
+++ huamr/entrypoints/grpo_s2s_2.py
@@ -11,6 +11,7 @@ from datasets import Dataset, DatasetDict, load_dataset
 from dotmap import DotMap
 from huamr.data.amr3 import AMR3Dataset
 from huamr.entrypoints.train import calc_smatch_for_grpo
+from huamr.entrypoints.train_grpo_s2s import xfm_is_amr_valid
 from huamr.utils.config_reader import get_config_from_yaml
 from huamr.utils.langtype import LangType
 from torch import FloatTensor, LongTensor
